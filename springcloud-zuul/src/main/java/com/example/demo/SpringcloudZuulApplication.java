283c2b10c71eb09a1891ff181ff06d28c8583fa5
Te <2503147957@qq.com>
2018-12-12 15:47:47 +0800

    Add files via upload

--- springcloud-zuul/src/main/java/com/example/demo/SpringcloudZuulApplication.java
+++ springcloud-zuul/src/main/java/com/example/demo/SpringcloudZuulApplication.java
@@ -7,10 +7,12 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 import com.example.demo.filter.TokenFilter;
+import com.spring4all.swagger.EnableSwagger2Doc;
 
 @SpringBootApplication
 @EnableZuulProxy   //开启网关
 @EnableEurekaClient   //注册服务到eureka中
+@EnableSwagger2Doc
 public class SpringcloudZuulApplication {
 
 	public static void main(String[] args) {
@@ -21,4 +23,6 @@ public class SpringcloudZuulApplication {
 	public TokenFilter tokenFilter(){
 		return new TokenFilter();
 	}
+	//添加文档来源
+	
 }
